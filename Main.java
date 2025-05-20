4d84a9117fc3b7d645f7d638be4bc8c8ea3b3532
KimNganTP5813 <akinosora13@gmail.com>
2025-05-20 16:59:10 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Khai báo và khởi tạo thông tin cá nhân
+        String name = "Trần Phú Kim Ngân";
+        int age = 28;
+        String address = "Đà Lạt";
+        //In thông tin ra màn hình
+        System.out.printf("Tên tôi là %s, tôi %s tuổi, hiện tại đang học khóa TSUBASA, quê ở %s.", name, age, address);
+    }
+}
